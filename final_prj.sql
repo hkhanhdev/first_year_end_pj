65e09237f0cde18ec0b9687e8719bcb929b60d24
khanh <nguyenxuanhakhanh88060@gmail.com>
2023-08-14 19:05:54 +0700

    add product detail to database

--- final_prj.sql
+++ final_prj.sql
@@ -11,6 +11,24 @@ create table tbl_user (
     customer_email varchar(50) unique key,
     customer_message text
 );
+create table tbl_category (
+	cate_id int(11) primary key auto_increment,
+    cate_name varchar(255)
+);
+
+
+create table tbl_product(
+	prd_id int(11) primary key auto_increment,
+    prd_name varchar(255) not null,
+    prd_price decimal(8,0) not null,
+    prd_image varchar(255),
+    cate_id int(11) not null,
+    prd_ram varchar(255) not null,
+    prd_gpu varchar(255) not null,
+    prd_resolution varchar(255) not null,
+    prd_operating_system varchar(255) not null,
+    foreign key(cate_id) references tbl_category(cate_id)
+);
     
 create table tbl_order (
 	ord_id int(11) primary key auto_increment,
@@ -24,37 +42,20 @@ create table tbl_order (
 	foreign key(customer_id) references tbl_user(user_id)
 );
 
-
-create table tbl_category (
-	cate_id int(11) primary key auto_increment,
-    cate_name varchar(255)
-);
-
-
-create table tbl_product(
-	prd_id int(11) primary key auto_increment,
-    prd_name varchar(255) not null,
-    prd_price decimal(8,0) not null,
-    prd_image varchar(255),
-    cate_id int(11) not null,
-    foreign key(cate_id) references tbl_category(cate_id)
-);
-
-
 insert into tbl_category(cate_name)
 values
-('ASUS'),
+('Asus'),
 ('Macbook'),
 ('Dell'),
 ('HP');
 
-insert into tbl_product(prd_name,prd_price,prd_image,cate_id)
+insert into tbl_product(prd_name,prd_price,prd_image,cate_id,prd_ram,prd_gpu ,prd_resolution,prd_operating_system )
 values
-('Macbook Air 2022 13.6 inch Apple M2 – 16GB RAM 512GB SSD',700,'macbook.jpg',2),
-('Laptop Asus Vivobook M1403QA-LY022W R5 5600H/8GB/512GB',500,'img1.jpg',1),
-('Dell XPS 13 Plus 9320 (2022) - I7/32GB/1TB/UHD 4K Touch',550,'laptop-dell.jpg',3),
-('Laptop HP Pavilion 15 eg2062TU i3 1215U/8GB/256GB',600,'laptop-hp.jpg',4),
-('Macbook Air 2020 i3 8GB 256GB | MWTJ2/ MWTL2/ MWTK2',1900,'img2.jpg',2),
-('Laptop Dell Latitude 7390 2in1',450,'laptop-dell2.jpeg',3);
+('Macbook Air 2022 13.6 inch Apple M2 – 16GB RAM 512GB SSD',700,'macbook.jpg',2,"16GB","GPU 7 nhân, 16 nhân Neural Engine","2560 x 1600 pixels (2K)","MacOS"),
+('Laptop Asus Vivobook M1403QA-LY022W R5 5600H/8GB/512GB',500,'img1.jpg',1,"8GB","NVIDIA GeForce RTX 3050 4GB GDDR6, Boost Clock 1500MHz, TGP 85W","1920 x 1080 pixels (FullHD)","Windowns"),
+('Dell XPS 13 Plus 9320 (2022) - I7/32GB/1TB/UHD 4K Touch',550,'laptop-dell.jpg',3,"32GB","NVIDIA GeForce GTX 1650 4 GB GDDR6","1920 x 1080 pixels (FullHD)","Windowns"),
+('Laptop HP Pavilion 15 eg2062TU i3 1215U/8GB/256GB',600,'laptop-hp.jpg',4,"8GB","NVIDIA GeForce RTX 2050 4GB","1920 x 1080 pixels (FullHD)","Windowns"),
+('Macbook Air 2020 i3 8GB 256GB | MWTJ2/ MWTL2/ MWTK2',1900,'img2.jpg',2,"8GB","GPU 16 nhân, 16 nhân Neural Engine","2560 x 1600 pixels (2K)","MacOS"),
+('Laptop Dell Latitude 7390 2in1',450,'laptop-dell2.jpeg',3,"32GB","AMD Radeon Graphics","1920 x 1080 pixels (FullHD)","Windowns");
 
 
